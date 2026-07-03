0ada4299c0021e8b1e98de09eb421c1b7fca88f7
Ho Wa Ng <30599761+hw88hw88@users.noreply.github.com>
2026-07-03 22:59:27 +0800

    API yfinance-get financial data
    Uploading the code for getting financial data from yfinance API, and the testing for it.

---
+++ api_fin_data.py
@@ -0,0 +1,89 @@
+import os
+import pickle
+import time
+import json
+import yfinance as yf
+
+class APIFinData():
+    def __init__(self):
+        # store the file path of stock code
+        self.stock_symbol_file='CSV/stock_code.csv'
+        # store the file path of downloadable stock code
+        self.stock_symbol_file_downloadable='CSV/downloadable_stock_code.csv'
+        # store the log file path
+        self.json_filename='JSON/log.json'
+
+    # this function retrieve the symbols of constituent stocks of the Standard & Poor’s 500 (S&P500) on a specified date from a csv file
+    def get_symbol_from_csv(self):
+        symbol=[]
+        # Some stock code are invalid
+        # During the testing in 'test_all_sp500()' in test_fin_data.py, the stock code was split into 2 lists, i.e. downloadable and undownloadable. If there is a list of downloadable stocks, the list will be used. Otherwise, the original list of stock will be used.
+        if os.path.exists(self.stock_symbol_file_downloadable):
+            filename=self.stock_symbol_file_downloadable
+        else:
+            filename=self.stock_symbol_file
+        with open(filename) as f:
+            csv_str = f.read()
+            lines = csv_str.split('\n')
+            for line in lines:
+                tickers = line.split(',')
+                for ticker in tickers:
+                    if ticker != '':
+                        symbol.append(str(ticker))
+        return symbol
+
+    # get the financial data from external API
+    ## input:
+    ### symbol: the stock code e.g. "MSFT", "MU"
+    ## output:
+    ### raw_data: the dataframe from the external API
+    def get_financial_data(self, symbol):
+        # store the raw data in pickle file for later retrieval
+        pickle_filename='pickle/stock_data/'+symbol+'_max.pkl'
+        # store the data frame of raw data in csv file, because this is human readable form
+        df_csv_filename='CSV/stock_data/'+symbol+'_max.csv'
+
+        # # check if the data was saved in files
+        if not os.path.exists(pickle_filename):
+            # download the data from external source if not exist
+            raw_data = yf.download(symbol, period='max', auto_adjust=True)
+            # wait to avoid abuse the API
+            time.sleep(1)
+
+            # check if the raw data is empty
+            # For an empty data frame from yfinance, there are 7 elements
+            if raw_data is None or int(raw_data.size) < 1:
+                return None
+            if symbol=='ABC':
+                print('size= ', raw_data.size)
+
+            # convert the raw data to pickle format and
+            # save to pickle file
+            self.write_to_pickle_binary_file(filename=pickle_filename, data=raw_data)
+            # save to human readable format, CSV file
+            raw_data.to_csv(df_csv_filename)
+            # saving the log of the requests to external API        
+            self.to_json(to_json_content={'symbol': symbol} , filename=self.json_filename)
+        else:
+            # load data from file
+            raw_data=self.read_from_pickle_binary_file(filename=pickle_filename)
+        return raw_data
+
+    # converting data the json format and save to a file (append the content)
+    def to_json(self, to_json_content, filename):
+        content = json.dumps(to_json_content)
+        with open(filename, 'a') as f:
+            f.write(content)
+
+    # reading pickle from binary file
+    def read_from_pickle_binary_file(self, filename):
+        with open(filename, 'rb') as f:
+            data = f.read()
+        return pickle.loads(data)
+
+    # writing pickle to binary file
+    def write_to_pickle_binary_file(self, filename, data):
+        pickled_data=pickle.dumps(data, protocol=pickle.HIGHEST_PROTOCOL)
+        with open(filename, 'wb') as f:
+            f.write(pickled_data)
+
---
+++ test_fin_data.py
@@ -0,0 +1,96 @@
+# the code in this file is newly written to unit-test the code in 'fin_data.py'
+# the whole coding process followed the three laws of test driven development
+
+import unittest
+import api_fin_data
+import os
+import csv
+
+class TestAPIFinData(unittest.TestCase):
+    # test if the class, variables and functions can be created successfully or not
+    def test_class_functions(self):
+        self.assertIsNotNone(api_fin_data.APIFinData)
+        self.assertIsNotNone(api_fin_data.APIFinData())
+
+        get_fin_data=api_fin_data.APIFinData()
+        self.assertIsNotNone(get_fin_data.stock_symbol_file)
+        self.assertIsNotNone(get_fin_data.json_filename)
+        self.assertIsNotNone(get_fin_data.get_symbol_from_csv)
+        self.assertIsNotNone(get_fin_data.get_financial_data)
+        self.assertIsNotNone(get_fin_data.to_json)
+        self.assertIsNotNone(get_fin_data.read_from_pickle_binary_file)
+        self.assertIsNotNone(get_fin_data.write_to_pickle_binary_file)
+
+    # test if functions return something
+    def test_function_returns(self):
+        get_fin_data=api_fin_data.APIFinData()
+        self.assertIsNotNone(get_fin_data.get_symbol_from_csv())
+        # make request to external API
+        symbol='MSFT'
+        data=get_fin_data.get_financial_data(symbol=symbol)
+        self.assertIsNotNone(data)
+
+        symbol='ABC'
+        data=get_fin_data.get_financial_data(symbol=symbol)
+        self.assertIsNone(data)
+
+    # test if variables store correct type of data
+    def test_var_type(self):
+        get_fin_data=api_fin_data.APIFinData()
+        self.assertEqual(str(type(get_fin_data.stock_symbol_file)), "<class 'str'>")
+        self.assertEqual(str(type(get_fin_data.json_filename)), "<class 'str'>")
+
+    # test if get_financial_data() works as expected
+    def test_get_financial_data(self):
+        get_fin_data=api_fin_data.APIFinData()
+        # make request to external API
+        symbol='MSFT'
+        api_data=get_fin_data.get_financial_data(symbol=symbol)
+        self.assertIsNotNone(api_data)
+        # test if a pickle file was saved successfully, and test if the pickle can be read
+        data=get_fin_data.read_from_pickle_binary_file('pickle/stock_data/' + symbol + '_max.pkl')
+        self.assertIsNotNone(data)
+        self.assertEqual(str(type(data)), "<class 'pandas.DataFrame'>")
+
+        symbol='ABC'
+        api_data=get_fin_data.get_financial_data(symbol=symbol)
+        self.assertIsNone(api_data)
+        # test if a pickle file was saved successfully, and test if the pickle can be read
+        file_path='pickle/stock_data/' + symbol + '_max.pkl'
+        self.assertEqual(os.path.exists(file_path), False)
+
+    # test if all constituent stocks of S&P500 can be retrieved
+    # the result will be stored to two csv files ('downloadable_stock_code.csv' and 'undownloadable_stock_code.csv')
+    def test_all_sp500(self):
+        get_fin_data=api_fin_data.APIFinData()
+        # get all stock code
+        symbols = get_fin_data.get_symbol_from_csv()
+        downloadable = []
+        undownloadable = []
+        for s in symbols:
+            data=get_fin_data.get_financial_data(symbol=s)
+            if data is None:
+                undownloadable.append(s)
+            else:
+                downloadable.append(s)
+
+        # prevent overwrite the list of downloadable code
+        # the existance of file implies that the list was created
+        downloadable_filename='CSV/downloadable_stock_code.csv'
+        if not os.path.exists(downloadable_filename):
+            with open(downloadable_filename, 'w') as f:
+                writer=csv.writer(f)
+                writer.writerow(downloadable)
+
+        # prevent overwrite the list of undownloadable code
+        # the existance of file implies that the list was created
+        undownloadable_filename='CSV/undownloadable_stock_code.csv'
+        if not os.path.exists(undownloadable_filename):
+            with open(undownloadable_filename, 'w') as f:
+                writer=csv.writer(f)
+                writer.writerow(undownloadable)
+        
+        # check if lists created successfully
+        self.assertIsNotNone(os.path.exists(downloadable_filename))
+        self.assertIsNotNone(os.path.exists(undownloadable_filename))
+unittest.main()
