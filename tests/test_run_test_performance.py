c09f7b40714f818d6b22138c2e2f4f7bf6b43830
Ho Wa Ng <30599761+hw88hw88@users.noreply.github.com>
2026-08-19 20:19:13 +0800

    Delete tests/test_run_test_performance.py

--- tests/test_run_test_performance.py
+++
@@ -1,7 +0,0 @@
-import unittest
-
-
-class TestRunTestPerformance(unittest.TestCase):
-    # test if the class, variables and functions can be created successfully or not
-    def test_class_functions(self):
-        pass
